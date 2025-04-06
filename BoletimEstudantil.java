3df6caa03ae10e641ccb457e372ad5d0d474c61f
gitDio <ademarteiyuchina@gmail.com>
2025-04-06 14:40:06 -0300

    reajuste

---
+++ BoletimEstudantil.java
@@ -0,0 +1,12 @@
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal<6) 
+            System.out.println("reprovado");
+        else if(mediaFinal==6)
+            System.out.print("prova minerva");
+        else
+            System.out.print("aprovado");
+    }
+}
---
+++ MinhaClasse.java
@@ -0,0 +1,25 @@
+public class MinhaClasse {
+    public static void main (String [] args) {
+        System.out.print ( "ola moço");
+
+
+        int ano = 2021;
+        ano = 2025;
+
+        final String BR = "Brasil";
+
+        final double pi = 3.14;
+
+        boolean simNao = true;
+        simNao = false;
+        
+        String primeiroNome = "maria";
+        String segundoNome = "eduarda";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.print(nomeCompleto);
+
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "resultado do metodo" + primeiroNome.concat("").concat(segundoNome);
+    }
+}
